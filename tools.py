0527a217a2080f2e6baeb5f0779aa53d91e71845
Wisely <wisely12311@gmail.com>
2023-03-07 00:31:05 +0000

    tools addition

--- tools.py
+++ tools.py
@@ -77,3 +77,53 @@ def first_order_ECN(t, I, T, V_actual, ref_OCV, ref_SOC,
 
             update_I_R1(i, I_R1, t, I, R1_val, C1_val)  # update I_R1 at i+1
     return V_pred
+
+
+
+def first_order_ECN_temp(t, I, T, V_actual, ref_OCV, ref_SOC,
+                    fit_R0_temp, fit_R1_temp, fit_C1_temp, T_change):
+    """
+    The first order ECN model for part 2. t is time and T is temperature.
+    !!!!!
+    fit_R0, fit_R1, fit_C1 are placeholder functions for fitting R0, R1, C1.
+    They need to be defined before calling first_order_ECN().
+    They need to be changed as the model updating.
+    They are created to ensure first_order_ECN() can be used
+        everywhere throughout this project.
+    """
+    Q = 2500
+    eta = 1
+
+    N = len(t)
+    z = np.ndarray([N, 1])
+    V_pred = np.ndarray([N, 1])
+    OCV = np.ndarray([N, 1])
+    I_R1 = np.ndarray([N, 1])
+    if T is None:
+        # redundant array, to prevent error when calling T[i]
+        T = np.ndarray([N, 1])
+
+
+    z0 = match_val(V_actual[0], ref_OCV, ref_SOC)
+    z[0] = z0
+    I_R1[0] = 0
+
+
+    for i in range(N):
+        R1_val = fit_R1_temp(T[i])  # use values at i
+        R0_val = fit_R0_temp(I[i], T[i])
+        T_new = T_change(I[i], R0_val, R1_val, dt, T, T_env)
+
+        OCV[i] = match_val(z[i], ref_SOC, ref_OCV)
+        V_pred[i] = OCV[i] - R1_val * I_R1[i] - R0_val * I[i]
+
+        if i != N-1:
+            update_SOC(i, z, t, I, eta, Q)  # update z at i+1
+            # We are updating the next value (i+1):
+            R0_val = fit_R0_temp(I[i], T[i+1])
+            R1_val = fit_R1_temp(T[i+1])  # use values at i+1
+            C1_val = fit_C1_temp(I[i+1], z[i+1], T[i+1])
+
+            update_I_R1(i, I_R1, t, I, R1_val, C1_val)  # update I_R1 at i+1
+    return V_pred
+
